0b1fa3c19a07c114f19777cf140d78be8f1e0f2e
Alejandro Vela <avelaproyt@gmail.com>
2024-07-03 20:55:10 -0600

    Host on dev & prod

--- client/src/consts/hosts.tsx
+++ client/src/consts/hosts.tsx
@@ -1,5 +1,9 @@
 // export const API_HOST = "https://authapptest.onrender.com";
 // export const WEB_HOST = "https://satixx-auth.vercel.app";
 
-export const API_HOST = "http://localhost:3000";
-export const WEB_HOST = "http://localhost:5173";
+export const API_HOST = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : "https://authapptest.onrender.com";
+export const WEB_HOST = import.meta.env.DEV
+  ? "http://localhost:5173"
+  : "https://satixx-auth.vercel.app";
