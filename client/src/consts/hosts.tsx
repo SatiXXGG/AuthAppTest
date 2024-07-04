e3382d27a49f60ea36fd31e5ea33d2adf1b6da28
Alejandro Vela <avelaproyt@gmail.com>
2024-07-03 20:05:24 -0600

    fixed hosts

--- client/src/consts/hosts.tsx
+++ client/src/consts/hosts.tsx
@@ -1,2 +1,2 @@
-export const API_HOST = "https://authapptest.onrender.com/";
-export const WEB_HOST = "https://satixx-auth.vercel.app/";
+export const API_HOST = "https://authapptest.onrender.com";
+export const WEB_HOST = "https://satixx-auth.vercel.app";
