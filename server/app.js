64289c824815786e5aabb7aa454ba11755a01cba
Alejandro Vela <avelaproyt@gmail.com>
2024-07-03 19:59:44 -0600

    fixed io error

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ import User from "./controllers/User.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
 import { createClient } from "@libsql/client";
 // setup
 
@@ -44,56 +43,6 @@ app.get("/user", User.getUserData);
 app.patch("/user", User.patch);
 app.post("user/message");
 
-io.on("connection", async (socket) => {
-  console.log("user connected");
-
-  socket.on("message", async (message) => {
-    
-
-    try {
-      
-      const result = await chatDb.execute({
-        sql: "INSERT INTO messages (message, owner) VALUES (:message, :owner)",
-        args: {
-          message: message,
-          owner: socket.handshake.auth.username ?? "Anonymous",
-        },
-      })
-
-      socket.broadcast.emit("message", {
-      message: message,
-      owner: socket.handshake.auth.username ?? "Anonymous",
-      serverOffset: result.lastInsertRowid.toString()
-    });
-
-    } catch (e) {
-      console.log(e)
-    }
-  });
-
-  if (!socket.recovered) {
-    console.log("basic");
-    try {
-      const result = await chatDb.execute({
-        sql: "SELECT * FROM messages WHERE id > :offset",
-        args: {
-          offset: socket.handshake.auth.serverOffset ?? 0,
-        },
-      });
-
-      const messages = result.rows.map((row) => {
-        const { id, message, owner, created_at } = row;
-        socket.broadcast.emit("message", {
-          message: message,
-          owner: owner,
-          created_at: created_at,
-        })
-      })
-    } catch (e) {
-      console.log(e);
-    }
-  }
-});
 
 // protected routes
 
